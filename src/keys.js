3fe16b03dd3bfc46fcaf77a3ddc2e3e4f99e7408
Miruin <44851774+Miruin@users.noreply.github.com>
2021-04-22 07:48:01 -0400

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -2,7 +2,7 @@ module.exports = {
 
     mongodb: {
 
-        URI: 'mongodb+srv://cluster0.k5s7r.mongodb.net/myFirstDatabase'
+        URI: 'mongodb+srv://mongo:masterkeyd@cluster0.k5s7r.mongodb.net/clonMercadoLibre'
 
     }
 
