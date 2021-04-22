8f80554724e45bd79cca5e9a665ab6a4f9a8e03b
Miruin <josefariakike95@gmail.com>
2021-04-22 09:47:59 -0400

    Update

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { mongodb } = require('./keys');
 
-mongoose.connect(mongodb.URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.URI_MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(db => console.log('conectado'))
 .catch(err => console.log(err))
